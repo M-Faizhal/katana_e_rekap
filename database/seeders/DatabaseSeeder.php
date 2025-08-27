89f426614896c20a829c7903ada4565d42fab4c0
Muhamad Vicky Oktafrian <147964469+VickyOktafrian@users.noreply.github.com>
2025-08-27 18:53:55 +0700

    Add Super Administrator user to seeder
    A default Super Administrator user is now created in the DatabaseSeeder for testing purposes. The previous call to CompleteSystemSeeder and related output have been commented out.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -28,10 +29,22 @@ class DatabaseSeeder extends Seeder
         // DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         // Call seeders in proper order (respecting foreign key constraints)
-        $this->call([
-            CompleteSystemSeeder::class,
-        ]);
+        // $this->call([
+        //     CompleteSystemSeeder::class,
+        // ]);
 
-        echo "\nData lengkap telah dibuat untuk testing flow bisnis!\n";
+        // echo "\nData lengkap telah dibuat untuk testing flow bisnis!\n";
+
+
+        User::create([
+                'nama' => 'Super Administrator',
+                'username' => 'superadmin',
+                'email' => 'superadmin@katana.com',
+                'password' => Hash::make('K@mil6969'),
+                'role' => 'superadmin',
+                'no_telepon' => '081234567890',
+                'alamat' => 'Jakarta Pusat, DKI Jakarta'
+            ]
+        );
     }
 }
